ac0155f690cf0a3322df89df9ca7a8db83718632
joelfrewy <Joelfrewin@gmail.com>
2018-08-27 22:33:45 +0800

    friendly vendor ids

--- app/models/spree/pack.rb
+++ app/models/spree/pack.rb
@@ -1,4 +1,6 @@
-class Spree::Pack < ApplicationRecord
+module Spree
+  class Pack < Spree::Base
   acts_as_paranoid
   belongs_to :variant, class_name: "Spree::Variant"
+  end
 end
--- app/models/spree/vendor_decorator.rb
+++ app/models/spree/vendor_decorator.rb
@@ -1,20 +1,43 @@
-Spree::Vendor.class_eval do
-  before_create :set_slug
-  #before_action :get_vendor, only: [:show, :edit, :destroy]
-  has_one_attached :avatar, dependent: :destroy
-  belongs_to :user, foreign_key: "owner_id"
+module Spree
+  Vendor.class_eval do
+    extend FriendlyId
+    friendly_id :slug_candidates, use: :history
+    #before_action :get_vendor, only: [:show, :edit, :destroy]
+    has_one_attached :avatar, dependent: :destroy
+    belongs_to :user, foreign_key: "owner_id"
 
-  def to_param
-    self.slug
-  end
+    after_destroy :punch_slug
+    after_restore :update_slug_history
 
-  private
+    before_validation :normalize_slug, on: :update
 
-  def set_slug
-    self.slug = [name.parameterize].join("-")
-  end
+    validates :slug, presence: true, uniqueness: { allow_blank: false }
 
-  def get_vendor
-    @vendor = Vendor.find_by slug: params[:slug]
+    def normalize_slug
+      self.slug = normalize_friendly_id(slug)
+    end
+
+    def punch_slug
+      # punch slug with date prefix to allow reuse of original
+      update_column :slug, "#{Time.current.to_i}_#{slug}"[0..254] unless frozen?
+    end
+
+    def update_slug_history
+      save!
+    end
+
+    # Try building a slug based on the following fields in increasing order of specificity.
+    def slug_candidates
+      [
+          :name,
+          [:name, :id]
+      ]
+    end
+
+    private
+
+    def get_vendor
+      @vendor = Vendor.find_by slug: params[:slug]
+    end
   end
 end
